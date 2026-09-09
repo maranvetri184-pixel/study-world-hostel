31034e648d0e83e7a2bd4e7db2f8b98dc0323f9e
maranvetri184-pixel <maranvetri184@gmail.com>
2026-09-09 15:09:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,4 +113,5 @@ def gate():
 def logout(): session.clear(); return redirect('/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
